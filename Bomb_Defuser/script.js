256c1f14b99404902111b0b94ccd04db8bfb1dc3
rajesh-patnala <iamrajeshpatnala@gmail.com>
2023-06-07 15:49:56 +0530

    initial commit

---
+++ Bomb_Defuser/script.js
@@ -0,0 +1,27 @@
+let bombDefuserKey = document.getElementById("bombDefuserKey");
+let bombTimer = document.getElementById("bombTimer");
+
+let unqId = null;
+
+let count = parseInt(bombTimer.innerHTML);
+
+let countDown = function () {
+    count--;
+    bombTimer.innerHTML = count;
+    if (count === 0) {
+        bombTimer.innerHTML = "BOOM!";
+        clearInterval(unqId);
+    }
+};
+
+unqId = setInterval(countDown, 1000);
+
+bombDefuserKey.addEventListener("keydown", function (event) {
+    let defuseKey = bombDefuserKey.value;
+
+    if (defuseKey == "defuse" && count !== 0 && event.key === "Enter") {
+        bombTimer.innerHTML = "You did it!";
+        clearInterval(unqId);
+    }
+
+});
